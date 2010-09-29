27b60be89171ffae40e6dfe531f67b69c99acd43
rodrigo.zr <rodrigo.zr@f190bac4-7986-11de-adf2-cbd3851e0ccf>
2010-09-29 19:03:04 +0000

    v1.4.2
    git-svn-id: https://droidwall.googlecode.com/svn/trunk@93 f190bac4-7986-11de-adf2-cbd3851e0ccf

--- src/com/googlecode/droidwall/Api.java
+++ src/com/googlecode/droidwall/Api.java
@@ -54,7 +54,7 @@ import android.widget.Toast;
  */
 public final class Api {
 	/** application version string */
-	public static final String VERSION = "1.4.2-dev";
+	public static final String VERSION = "1.4.2";
 	/** special application UID used to indicate "any application" */
 	public static final int SPECIAL_UID_ANY	= -10;
 	/** root script filename */
